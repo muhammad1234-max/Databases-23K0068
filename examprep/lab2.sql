051277c495997b72269ffb8151155c81cf85616e
Muhammad Abbas <75746019+muhammad1234-max@users.noreply.github.com>
2025-10-08 17:32:14 +0500

    Update lab2.sql

--- examprep/lab2.sql
+++ examprep/lab2.sql
@@ -208,3 +208,30 @@ FROM employees;
 
 
 
+-- IN – LAB TASKS
+-- Q1. Find the total salary of all employees.
+-- Q2. Find the average salary of employees.
+-- Q3. Count the number of employees reporting to each manager.
+-- Q4. Select * employees who has lowest salary.
+-- Q5. Display the current system date in the format DD-MM-YYYY.
+-- Q6. Display the current system date with full day, month, and year (e.g.,
+-- MONDAY AUGUST 2025).
+-- Q7. Find all employees hired on a Wednesday.
+-- Q8. Calculate months between 01-JAN-2025 and 01-OCT-2024.
+-- Q9. Find how many months each employee has worked in the company (using
+-- hire_date).
+-- Q10.Extract the first 5 characters from each employee’s last name.
+
+-- Post– LAB TASKS
+
+-- Q11. Pad employee first names with * on the left side to make them 15 characters wide.
+-- Q12. Remove leading spaces from &#39; Oracle&#39;.
+-- Q13. Display each employee’s name with the first letter capitalized.
+-- Q14. Find the next Monday after 20-AUG-2022.
+-- Q15. Convert &#39;25-DEC-2023&#39; (string) to a date and display it in MM-YYYY format.
+-- Q16. Display all distinct salaries in ascending order.
+-- Q17. Display the salary of each employee rounded to the nearest hundred.
+-- Q18. Find the department that has the maximum number of employees.
+-- Q19. Find the top 3 highest-paid departments by total salary expense.
+-- Q20. Find the department that has the maximum number of employees.
+
